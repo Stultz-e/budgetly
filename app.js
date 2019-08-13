929886d72b91e756cac4f1ba045db30db1bc2bac
Chris <cstultz@oswego.edu>
2019-08-13 16:39:52 -0500

    shoenice

---
+++ app.js
@@ -0,0 +1,20 @@
+// BUDGET CONTROLLER
+var budgetController = (function() {
+
+    // Some code
+
+})(); 
+
+// UI CONTROLLER
+var UIController = (function() {
+
+    // Some code
+
+}) ();
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+
+    Document.querySelector('')
+
+}) (budgetController, UIController);
